5e11db3193c40e677c80b055f5d7a51c16a23ebf
Phan Anh Đức <55018920+ducphan99@users.noreply.github.com>
2021-10-03 23:32:13 +0700

    Delete Website_ShopDunk directory

--- Website_ShopDunk/App/database/Database.php
+++
@@ -1,6 +0,0 @@
-<?php
-    class Database{
-        const DB_DSN='mysql:host=localhost;port=3306;dbname=PHP1220E2_nhom1;charset=utf8';
-        const DB_USERNAME = 'PHP1220E2_nhom1';
-        const DB_PASSWORD = '111111';
-    }
--- Website_ShopDunk/Mvc/Backend/views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Phan Anh Đức </b>
-    </div>
-    <strong>Copyright &copy; Phan Anh Đức   </strong>
-</footer>
